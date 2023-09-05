1cb6c8367d764fee309d9e488cbd996accabeed4
Heidi <osburnhl@s.dcsdk12.org>
2023-09-05 15:47:23 -0600

    21-8-2023

---
+++ scripts/Basics-1.js
@@ -0,0 +1,13 @@
+
+//console.log("Hello world");
+
+let firstname = "Heidi";
+let age = 13;
+let agestring = "13";
+let favoritebook = 'Red White and Royal Blue';
+
+let isGameOver = false;
+
+let fruits = ["Apple", "Grapes", "Banana"];
+
+console.log(fruits[2]);
---
+++ scripts/advice-generator.js
@@ -0,0 +1,44 @@
+let advisors = [
+    "A wise man once told me",
+    "Mother always told me",
+    "My uncle Rupert once said",
+    "a little birdy once told me"
+];
+
+let howOften = [
+    "Never",
+    "Always",
+    "As often as you can",
+    "Never ever"
+];
+
+let verbs = [
+    "run with",
+    "swim in",
+    "eat a",
+    "climb on",
+    "dance with"
+];
+
+let adjetives =[
+    "spiky",
+    "furry",
+    "Funny",
+    "Spicey",
+    "odd",
+    "loud",
+    "scary"
+];
+
+let nouns =[
+    "desks",
+    "nuclear bombs",
+    "Humans",
+    "Computers",
+    "video games",
+    "Commuters"
+];
+
+let rand = math.floor(Math.random()*100);
+
+console.log(rand);
